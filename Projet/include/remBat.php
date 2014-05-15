8b0ca8f7037273efde8ed96233a103d67426a341
hyalis <hyalis81@gmail.com>
2014-05-15 16:21:39 +0200

    Supression des fichier de include

--- Projet/include/remBat.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "bdd.php";
-	
-	$idBatiment = $_GET['idBatiment'];
-	$resultats=$connection->query("DELETE FROM batiment WHERE idBatiment = $idBatiment");
-	
-	include "listBat.php";
-	$resultats->closeCursor();
-?>
--- Projet/include/remCap.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include "bdd.php";
-	
-	$idCapteur = $_GET['idCap'];
-	$idPiece = $_GET['idPiece'];
-	$resultats=$connection->query("DELETE FROM Localiser WHERE Capteur_idCapteur = $idCapteur");
-	$resultats=$connection->query("DELETE FROM Capteur WHERE idCapteur = $idCapteur");
-	header('Location: ../index.php?p=editS&idPiece='.$idPiece); 
-?>
