bfa7b1ce867f8c36f1c410b75740ecd27bb758ca
hyalis <hyalis81@gmail.com>
2014-05-19 14:23:10 +0200

    Mise en cohérence

--- Projet/include/Forms/Pie/editP.php
+++ Projet/include/Forms/Pie/editP.php
@@ -48,9 +48,6 @@
 
 <div class="row">
 	<div class="col-lg-12">
-<<<<<<< HEAD
-		<h1>Edit Rooms <small>Edit rooms of </small></h1>
-=======
 		<h1>Edit Rooms 
 			<small> Edit rooms of #<?php 
 				include "./bdd.php";
@@ -60,7 +57,6 @@
 				echo $resultat->nom ; ?>
 			</small>
 		</h1>
->>>>>>> 0155c5baffca44a1b07c38ec7d90539aa332927a
 		<ol class="breadcrumb">
 			<li><a href="index.php?p=Forms/Bat/editB"><i class="fa fa-edit"></i> Edit building</a></li>
 			<li class="active"><i class="fa fa-home"></i> Edit rooms</li>
