684c6d333b7bbeebbee4405a78a25f0b82f70a8c
damienparise <damienparise82@gmail.com>
2014-06-04 10:03:33 +0200

    Ajouter onglet Sensor et Expérience pour le tree select

--- Projet/oracle.php
+++ Projet/oracle.php
@@ -16,19 +16,18 @@
 	$motdepasse = 'master1';
 
 	$lien_base =
-	"oci:dbname=(DESCRIPTION = 
-	(ADDRESS_LIST = 
-	(ADDRESS = 
-	(PROTOCOL = TCP)
-	(Host = ".$hote .")
-	(Port = ".$port."))
-	)
-	(CONNECT_DATA =
-	(SERVER = DEDICATED)
-	(SERVICE_NAME = ".$service.")
-	)
+		"oci:dbname=(DESCRIPTION = 
+			(ADDRESS_LIST = 
+				(ADDRESS = 
+					(PROTOCOL = TCP)
+					(Host = ".$hote .")
+					(Port = ".$port."))
+			)
+			(CONNECT_DATA =
+				(SERVICE_NAME = ".$service.")
+		)
 	)";
-	
+	/*
 	 $lien_base =  "oci:dbname=(DESCRIPTION =
         (ADDRESS_LIST =
             (ADDRESS = (PROTOCOL = TCP)(HOST = 192.220.60.14)(PORT = 1522))
@@ -36,15 +35,16 @@
             (SID = ETUQUA) (SERVER = DEDICATED)
         )
     )";
-
+*/
 
 	try
 	{
 		// connexion à la base Oracle et création de l'objet
 		//$connexion = new PDO($lien_base, $utilisateur, $motdepasse);
-		$connexion = new PDO("oci:dbname=//195.220.60.14:1522/ETUQUA",$utilisateur, $motdepasse);
+		$connexion = new PDO($lien_base,$utilisateur, $motdepasse);
 	}catch (PDOException $erreur){
 		echo $erreur->getMessage() . "<br>";
 	}
-	//phpinfo();
+	phpinfo();
+	
 ?>
